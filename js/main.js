85b85418be80473ad0f6e999e0d7514a74afb023
v-prestigiacomo <114248896+v-prestigiacomo@users.noreply.github.com>
2022-10-27 17:56:17 +0100

    fixed contact form and validation

--- js/main.js
+++ js/main.js
@@ -17,16 +17,152 @@ function openNav() {
   }
 
 
-  function validateForm() {
-    let x = document.forms["contactForm"]["fname"].value;
-    let y = document.forms["contactForm"]["lname"].value;
-    let z = document.forms["contactForm"]["subject"].value;
-    if (x == "" || y == "") {
-      alert("Please fill in your name");
-      return false;
-    }
+// Form Validation
+// selector of each element in the form
+const firstNameEl = document.querySelector('#fname');
+const lastNameEl = document.querySelector('#lname');
+const emailEl = document.querySelector('#email')
+const subjectEl = document.querySelector('#subject');
+const messageEl = document.querySelector('#message');
 
-    if (z == "") {
-      alert("Please fill in the subject area")
-    }
+//main form selector
+const form = document.querySelector('#contact');
+
+
+
+//required field function
+const isRequired = value => value === '' ? false : true;
+
+
+// min and max length check function
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+// checks if email is valid
+const isEmailValid = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+
+// function that applies error styles and error message
+const showError = (input, message) => {
+  // get the form-field element
+  const formField = input.parentElement;
+  // add the error class
+  formField.classList.remove('success');
+  formField.classList.add('error');
+
+  // show the error message
+  const error = formField.querySelector('small');
+  error.textContent = message;
+};
+
+// function that applies success styles and hides the error message
+const showSuccess = (input) => {
+  // get the form-field element
+  const formField = input.parentElement;
+
+  // remove the error class
+  formField.classList.remove('error');
+  formField.classList.add('success');
+
+  // hide the error message
+  const error = formField.querySelector('small');
+  error.textContent = '';
+}
+
+// function that validates the first name value based on min and max length and if it is blank or not
+const checkFirstName = () => {
+
+  let valid = false;
+  const min = 1,
+      max = 20;
+  const firstName = firstNameEl.value.trim();
+
+  if (!isRequired(firstName)) {
+      showError(firstNameEl, 'First name cannot be blank.');
+  } else if (!isBetween(firstName.length, min, max)) {
+      showError(firstNameEl, `First name must be between ${min} and ${max} characters.`)
+  } else {
+      showSuccess(firstNameEl);
+      valid = true;
   }
+  return valid;
+}
+
+// function that returns true if the subject is provided and valid
+const checkSubject = () => {
+
+  let valid = false;
+  const min = 1,
+      max = 200;
+  const subject = subjectEl.value.trim();
+
+  if (!isRequired(subject)) {
+      showError(subjectEl, 'Subject cannot be blank.');
+  } else if (!isBetween(subject.length, min, max)) {
+      showError(subjectEl, `Subject must be between ${min} and ${max} characters.`)
+  } else {
+      showSuccess(subjectEl);
+      valid = true;
+  }
+  return valid;
+}
+
+
+// function that returns true if the email is provided and valid
+const checkEmail = () => {
+  let valid = false;
+  const email = emailEl.value.trim();
+  if (!isRequired(email)) {
+      showError(emailEl, 'Email cannot be blank.');
+  } else if (!isEmailValid(email)) {
+      showError(emailEl, 'Email is not valid.')
+  } else {
+      showSuccess(emailEl);
+      valid = true;
+  }
+  return valid;
+}
+
+// function that returns true if the message is provided and valid
+const checkMessage = () => {
+
+  let valid = false;
+  const min = 10,
+      max = 500;
+  const message = messageEl.value.trim();
+
+  if (!isRequired(message)) {
+      showError(messageEl, 'Message cannot be blank.');
+  } else if (!isBetween(message.length, min, max)) {
+      showError(messageEl, `Message must be between ${min} and ${max} characters.`)
+  } else {
+      showSuccess(messageEl);
+      valid = true;
+  }
+  return valid;
+}
+
+
+// event listener which stops the submit button from submitting then calls the validation functions
+form.addEventListener('submit', function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
+
+  // validate forms
+  let isFirstNameValid = checkFirstName(),
+      isEmailValid = checkEmail(),
+      isSubjectValid = checkSubject(),
+      isMessageValid = checkMessage();
+
+  let isFormValid = isFirstNameValid &&
+      isLastNameValid &&
+      isEmailValid;
+
+  // submit to the server if the form is valid
+  if (isFormValid) {
+
+  }
+});
+
